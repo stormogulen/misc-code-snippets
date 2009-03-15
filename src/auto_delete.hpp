b02d52cb9a7089c107adf32e8ddbd23e7ec475bf
Ulf Ejlertsson <ulf@ulf-ejlertssons-macbook.local>
2009-03-15 15:40:27 +0100

    Implemented AutoDelete.

--- src/auto_delete.hpp
+++ src/auto_delete.hpp
@@ -1,1 +1,24 @@
-//
+// -----------------------------------------------------------------------------
+// auto_delete.hpp 
+// -----------------------------------------------------------------------------
+// 
+// (C) Copyright Ulf Ejlertsson 2009.
+// 
+
+#ifndef __SRC_AUTO_DELETE_HPP_INCLUDED__
+#define __SRC_AUTO_DELETE_HPP_INCLUDED__
+
+template <typename T>
+class AutoDelete : boost::noncopyable
+{
+ public:
+    AutoDelete(T* p=0) : ptr_(p) { }
+    ~AutoDelete() throw() { delete ptr_; }
+
+ private:
+    T* ptr_;
+};
+
+#endif // !__SRC_AUTO_DELETE_HPP_INCLUDED__
+
+/* End of File */
