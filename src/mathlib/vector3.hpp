bc105db1e856f00f9b1d1d68b811964ae167438f
Ulf Ejlertsson <ulf@ulf-ejlertssons-macbook.local>
2009-04-07 23:00:48 +0200

    additions to Vector3 interface

--- src/mathlib/vector3.hpp
+++ src/mathlib/vector3.hpp
@@ -56,17 +56,36 @@ struct API_ENTRY Vector3
         return *this;
     }
 
+    //! Set all elements of a 2-D vector to the same scalar value.
+    explicit Vector3(T scalar)
+    {
+        x = scalar;
+        y = scalar;
+        z = scalar;
+    }
+
+    //! Subscripting operator to set or get an element.
     INLINE T& operator[](unsigned int idx)       { return m[i]; }
+
+    //! Subscripting operator to get an element.
     INLINE T  operator[](unsigned int idx) const { return m[i]; }
 
     //! Vector length.
-    T length() const;
+    T length() const
+    {
+        return mathaux::sqrt(x*x + y*y + z*z);
+    }
 
     //! Vector length squared.
-    T length_squared() const;
+    T length_squared() const
+    {
+        return (x*x + y*y + z*z);
+    }
 
-    //!
+    //! Equality operator.
     bool operator==(const Vector3<T>& rhs) const;
+
+    //! Not equal operator.
     bool operator!=(const Vector3<T>& rhs) const;
 
     //!
@@ -87,17 +106,48 @@ struct API_ENTRY Vector3
     //!
     void normalize();
 
+    //!
+    Vector3<T> operator+(const Vector3<T>& rhs) const;
+
+    //!
+    Vector3<T>& operator-(const Vector3<T>& rhs) const;
+
+    //!
+    Vector3<T> operator-() const;
+
     
     
 };
 
 //! Point distance.
 template <typename T>
-T distance(const Vector3<T>& v1, const Vector3<T>& v2);
+T distance(const Vector3<T>& v1, const Vector3<T>& v2)
+{
+    T x = v1.x - v2.x;
+    T y = v1.y - v2.y;
+    T z = v1.z - v2.z;
+
+    return mathaux::sqrt(x*x + y*y + z*z);
+}
 
 //! Point distance squared.
 template <typename T>
-T distance_squared(const Vector3<T>& v1, const Vector3<T>& v2);
+T distance_squared(const Vector3<T>& v1, const Vector3<T>& v2)
+{
+    T x = v1.x - v2.x;
+    T y = v1.y - v2.y;
+    T z = v1.z - v2.z;
+
+    return (x*x + y*y + z*z);
+}
+
+//!
+template <typename T>
+Vector3<T>& operator+=(Vector3<T>& self, const Vector3<T>& other);
+
+//!
+Vector3<T>& operator-=(Vector3<T>& self, const Vector3<T>& other);
+
 
 #endif // !__MATHLIB_VECTOR3_HPP_INCLUDED__
 
