0d6d21cf8a1edb95c93271788c7e0508b1e0af41
Ulf Ejlertsson <ulf@ulf-ejlertssons-macbook.local>
2009-04-07 20:42:38 +0200

    Additions to Vector3's interface

--- src/mathlib/vector3.hpp
+++ src/mathlib/vector3.hpp
@@ -55,7 +55,49 @@ struct API_ENTRY Vector3
 
         return *this;
     }
+
+    INLINE T& operator[](unsigned int idx)       { return m[i]; }
+    INLINE T  operator[](unsigned int idx) const { return m[i]; }
+
+    //! Vector length.
+    T length() const;
+
+    //! Vector length squared.
+    T length_squared() const;
+
+    //!
+    bool operator==(const Vector3<T>& rhs) const;
+    bool operator!=(const Vector3<T>& rhs) const;
+
+    //!
+    bool is_zero() const;
+
+    //!
+    bool is_unit() const;
+
+    //!
+    INLINE void set(T tx, T ty, T tz) { x = tx; y = ty; z = tz; }
+
+    //!
+    void clean();
+
+    //!
+    INLINE void zero() { x = y = z = 0; }
+
+    //!
+    void normalize();
+
+    
+    
 };
+
+//! Point distance.
+template <typename T>
+T distance(const Vector3<T>& v1, const Vector3<T>& v2);
+
+//! Point distance squared.
+template <typename T>
+T distance_squared(const Vector3<T>& v1, const Vector3<T>& v2);
 
 #endif // !__MATHLIB_VECTOR3_HPP_INCLUDED__
 
