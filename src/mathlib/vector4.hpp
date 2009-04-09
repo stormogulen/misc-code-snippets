cd34a024a2739c2db04dbbaea070d281f1849efa
Ulf Ejlertsson <ulf@ulf-ejlertssons-macbook.local>
2009-04-09 14:31:35 +0200

    Additions to Vector4's interface

--- src/mathlib/vector4.hpp
+++ src/mathlib/vector4.hpp
@@ -33,16 +33,100 @@ struct API_ENTRY Vector4
     Vector4() { }
 
     //! Construct
-    Vector4(T tx, T ty, T tz, T tw);
+    Vector4(T tx, T ty, T tz, T tw)
+        : x(tx), y(ty), z(tz), w(tw)
+    {
+    }    
 
     //! Destructor.
-    ~Vector4();
+    ~Vector4() { }
 
     //! Copy
-    Vector4(const Vector4<T>& rhs);
+    Vector4(const Vector4<T>& rhs)
+        : x(rhs.x),
+          y(rhs.y),
+          z(rhs.z),
+          w(rhs.w)
+    {
+    }
 
     //! Assignment
-    Vector4<T>& operator=(const Vector4<T>& rhs);
+    Vector4<T>& operator=(const Vector4<T>& rhs)
+    {
+        if (this == &rhs)
+            return *this;
+
+        x = rhs.x;
+        y = rhs.y;
+        z = rhs.z;
+        w = rhs.w;
+
+        return *this;
+    }
+
+    //!
+    INLINE T& operator[](unsigned int idx)       { return m[idx]; }
+
+    //!
+    INLINE T  operator[](unsigned int idx) const { return m[idx]; }
+
+    //! Vector length.
+    T length() const
+    {
+        return mathaux::sqrt(x*x + y*y + z*z + w*w);
+    }
+
+    //! Vector length squared.
+    /** Avoids square root. */
+    T length_squared() const
+    {
+        return (x*x + y*y + z*z + w*w);
+    }
+
+    //! Comparison operator.
+    bool operator==(const Vector4<T>& other) const
+    {
+        if (mathaux::are_equal(other.x, x) &&
+            mathaux::are_equal(other.y, y) &&
+            mathaux::are_equal(other.z, z) &&
+            mathaux::are_equal(other.w, w))
+            return true;
+
+        return false;
+    }
+
+    //! Comparison
+    bool operator!=(const Vector4<T>& other) const
+    {
+        if (mathaux::are_equal(other.x, x) &&
+            mathaux::are_equal(other.y, y) &&
+            mathaux::are_equal(other.z, z) &&
+            mathaux::are_equal(other.w, w))
+            return false;
+
+        return true;
+    }
+    
+    //! Check for zero vector.
+    bool is_zero() const
+    {
+        return mathaux::is_zero(x*x + y*y + z*z + w*w);
+    }
+
+    //! Check for unit vector.
+    bool is_unit() const
+    {
+        return mathaux::is_zero(1.0f - x*x - - y*y - z*z - w*w);
+    }
+
+    //! Set elements.
+    INLINE void set(T tx, T ty, T tz, T tw)
+    {
+        x = tx;
+        y = ty;
+        z = tz;
+        w = tw;
+    }
     
 
 };
